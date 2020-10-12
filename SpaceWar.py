7b137ea587404f389d99b102b7b287f7b1837164
Chahak Jadon <chahakjadon31@gmail.com>
2020-10-12 23:56:45 +0530

    updated SpaceWar.py

--- SpaceWar.py
+++ SpaceWar.py
@@ -1,107 +1,105 @@
+import os
 import turtle
 import math
 import random
 
-#create a screen
-wn = turtle.Screen()
-wn.bgcolor("black")
+#sets the animation speed to the maximum
+turtle.speed(0)
+turtle.bgcolor("black")
+#hide the default turtle
+turtle.ht()
+#This saves memory
+turtle.setundobuffer(1)
+#This speeds up drawing
+turtle.tracer(1)
 
-#create a player
-player = turtle.Turtle()
-player.penup()
-player.color("green")
-player.shape("turtle")
+#Child of the turtle class
+class Sprite(turtle.Turtle):
+    #constructor
+    def __init__(self, spriteshape, color, startx, starty):
+        turtle.Turtle.__init__(self, shape=spriteshape)
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.goto(startx, starty)
 
-#Draw boundary
-boundary = turtle.Turtle()
-boundary.penup()
-boundary.color("white")
-boundary.setposition(-300,-300)
-boundary.speed(0)
-boundary.pendown()
-boundary.pensize(3)
-for i in range(4):
-    boundary.forward(600)
-    boundary.left(90)
-boundary.hideturtle()
+        self.speed = 1
 
-speed = 1
+    def move(self):
+        self.fd(self.speed)
 
-#create a goal
-goal = turtle.Turtle()
-goal.color("white")
-goal.shape("square")
-goal.penup()
-goal.speed(0)
-goal.setposition(random.randint(-300,300),random.randint(-300,300))
+        #Boundary detection
+        if self.xcor()>290:
+            self.setx(290)
+            self.rt(60)
+        if self.xcor()<-290:
+            self.setx(-290)
+            self.rt(60)
+        if self.ycor()>290:
+            self.sety(290)
+            self.rt(60)
+        if self.ycor()<-290:
+            self.sety(-290)
+            self.rt(60)
 
-#define functions
-def movefw():
-    player.forward(20)
+#Child of Sprite class
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed=4
+        self.lives=3
 
-def moveleft():
-    player.left(30)
+    def turn_left(self):
+        self.lt(45)
 
-def moveright():
-    player.right(30)
+    def turn_right(self):
+        self.rt(45)
 
-"""
-def movebw():
-    player.forward(-40)
-"""
+    def accelerate(self):
+        self.speed+=1
 
-def isCollision(ply, gl):
-    d = math.sqrt(math.pow(ply.xcor()-gl.xcor(),2)+math.pow(ply.ycor()-gl.ycor(),2))
-    if d<20:
-        return True
-    else:
-        return False
+    def decelerate(self):
+        self.speed-=1
 
-def fire():
-    if turtle.onkey == "Down":
-        return True
+class Game():
+    def __init__(self):
+        self.level=1
+        self.score=0
+        self.state="Playing"
+        self.pen = turtle.Turtle()
+        self.lives=3
 
-#set Keyboard bindings
+    def draw_border(self):
+        #Draw border
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300,300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+
+#Create game object
+game = Game()
+
+#Draw the game border
+game.draw_border()
+#create my sprites
+player = Player("triangle","white", 0,0)
+
+#Keyboard bindings
 turtle.listen()
-turtle.onkey(movefw,"Up")
-turtle.onkey(moveleft,"Left")
-turtle.onkey(moveright,"Right")
-#turtle.onkey(movebw,"Down")
-turtle.onkey(fire,"Down")
+turtle.onkey(player.turn_left,"Left")
+turtle.onkey(player.turn_right,"Right")
+turtle.onkey(player.accelerate,"Up")
+turtle.onkey(player.decelerate,"Down")
 
+#Main game loop
 while True:
-    player.forward(speed)
+    player.move()
 
-    if player.xcor()>300 or player.xcor()<-300:
-        player.left(180)
-
-    if player.ycor()>300 or player.ycor()<-300:
-        player.left(180)
-
-    goal.forward(2)
-
-    if goal.xcor()>300 or goal.xcor()<-300:
-        goal.left(180)
-
-    if goal.ycor()>300 or goal.ycor()<-300:
-        goal.left(180)
-
-    if isCollision(player, goal):
-        goal.setposition(random.randint(-300,300),random.randint(-300,300))
-        goal.left(random.randint(0,360))
-
-    if fire():
-        #create a fire turtle
-        fireturtle = turtle.Turtle()
-        fireturtle.penup()
-        fireturtle.speed(0)
-        fireturtle.color("red")
-        fireturtle.shape("triangle")
-        fireturtle.setposition(player.xcor(),player.ycor())
-        fireturtle.pendown()
-        fireturtle.stamp()
-        fireturtle.forward(100)
-        fireturtle.hideturtle()
-
-
-        
+delay = input("Press enter to finish")
